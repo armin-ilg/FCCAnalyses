9489934aaafb22788ec2d24326030c4901218f81
github-action-benchmark <github@users.noreply.github.com>
2022-09-30 07:55:09 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 34dda34bd409ef12cef90bd9f2b0c187321dab5c

---
+++ dev/bench/data.js
@@ -0,0 +1,51 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1664524509302,
+  "repoUrl": "https://github.com/armin-ilg/FCCAnalyses",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "armin.fehr@lhep.unibe.ch",
+            "name": "Armin Fehr"
+          },
+          "committer": {
+            "email": "armin.fehr@lhep.unibe.ch",
+            "name": "Armin Fehr"
+          },
+          "distinct": true,
+          "id": "34dda34bd409ef12cef90bd9f2b0c187321dab5c",
+          "message": "Fixing merge",
+          "timestamp": "2022-09-30T09:44:15+02:00",
+          "tree_id": "5b97f0262af55a8fbe0f2c108f704ec1d7b43125",
+          "url": "https://github.com/armin-ilg/FCCAnalyses/commit/34dda34bd409ef12cef90bd9f2b0c187321dab5c"
+        },
+        "date": 1664524508270,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.88838791847229,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 18.87859797477722,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 11.677496194839478,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      }
+    ]
+  }
+}
