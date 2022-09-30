465382d82b7a169ba8686659110d1fa10413a58e
github-action-benchmark <github@users.noreply.github.com>
2022-09-30 07:56:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 34dda34bd409ef12cef90bd9f2b0c187321dab5c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664524572351,
+  "lastUpdate": 1664524574059,
   "repoUrl": "https://github.com/armin-ilg/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -123,6 +123,43 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "armin.fehr@lhep.unibe.ch",
+            "name": "Armin Fehr"
+          },
+          "committer": {
+            "email": "armin.fehr@lhep.unibe.ch",
+            "name": "Armin Fehr"
+          },
+          "distinct": true,
+          "id": "34dda34bd409ef12cef90bd9f2b0c187321dab5c",
+          "message": "Fixing merge",
+          "timestamp": "2022-09-30T09:44:15+02:00",
+          "tree_id": "5b97f0262af55a8fbe0f2c108f704ec1d7b43125",
+          "url": "https://github.com/armin-ilg/FCCAnalyses/commit/34dda34bd409ef12cef90bd9f2b0c187321dab5c"
+        },
+        "date": 1664524573456,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.774798539986056,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.963261012778936,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test",
+            "value": 7.4434723535595415,
+            "unit": "Evt/s"
           }
         ]
       }
